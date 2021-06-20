f59c8db7c84328af7bc51c0d2ba149d926acbbd4
Tmuat <Thomas_Muat@hotmail.com>
2021-06-20 11:40:47 +0000

    Adjusted carousel on contact page, including putting inline js into its own file

---
+++ assets/js/contact.js
@@ -0,0 +1,17 @@
+const Config = {
+  type: 'carousel',
+  startAt: 0,
+  perView: 3,
+  focusAt: 0,
+  autoplay: 4000,
+  hoverpause: true,
+  breakpoints: {
+    1200: {
+      perView: 2
+    },
+    992: {
+      perView: 1
+    }
+  }
+}
+new Glide('.glide', Config).mount()
